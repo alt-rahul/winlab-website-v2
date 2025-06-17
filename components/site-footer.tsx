89d9241554a70eb754f2be5392dfda1b4bf27c46
Rahul Rajkumar <rahul.rajkumar@rutgers.edu>
2025-06-17 22:46:53 +0000

    correct the date on footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,7 +2,7 @@ export function SiteFooter() {
   return (
     <footer className="py-12 px-4 bg-white dark:bg-black">
       <div className="container mx-auto text-center text-gray-500 dark:text-gray-500">
-        <p className="text-sm font-light">&copy; 2024 Edge Computing Research. All rights reserved.</p>
+        <p className="text-sm font-light">&copy; 2025 5G Edge Cloud Applications. All rights reserved.</p>
       </div>
     </footer>
   )
