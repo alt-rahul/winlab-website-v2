3dc011995f00018895b6f8a4269d6595ea075a8b
Rahul Rajkumar <rahul.rajkumar@rutgers.edu>
2025-06-17 23:14:46 +0000

    all packages are good

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,12 @@
-"use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+"use client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes";
+import { ReactNode } from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+interface Props extends ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children, ...props }: Props) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
